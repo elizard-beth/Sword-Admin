5837a051ef1c81df918a8c8c70924681ab93dbea
elizard-beth <71954340+elizard-beth@users.noreply.github.com>
2023-04-13 19:32:07 +1200

    Create Fly.lua

---
+++ src/Client_/Fly.lua
@@ -0,0 +1,3 @@
+-- unfortunately, i did not make this! it's an adapted version of kohl's fly script that was created all the way back in 2014.
+-- it's very ... 2014 ... and lacks many features modern fly scripts have
+-- it's buggy, i'll admit, more so even to the 2014 version. but it works.
